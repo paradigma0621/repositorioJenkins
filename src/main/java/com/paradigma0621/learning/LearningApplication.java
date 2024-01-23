1995ef152b290521745982ed1c4aea114ffa271e
mselucas <lucas.borsatto@nexti.com>
2024-01-23 16:29:49 -0300

    Adicionado comentário

--- src/main/java/com/paradigma0621/learning/LearningApplication.java
+++ src/main/java/com/paradigma0621/learning/LearningApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningApplication {
-
+//aa
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 	}
