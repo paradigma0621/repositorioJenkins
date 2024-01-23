bbbbdd4642a03c2100b5b08d9292861b431af8f1
mselucas <lucas.borsatto@nexti.com>
2024-01-23 15:39:42 -0300

    Adicionados testes, todos passando

---
+++ src/test/java/com/paradigma0621/learning/LearningApplicationTest.java
@@ -0,0 +1,43 @@
+package com.paradigma0621.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class LearningApplicationTest {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+
+	@Test
+	void contextLoads2() {
+		assertTrue(true);
+	}
+
+	@Test
+	void contextLoads3() {
+		assertTrue(true);
+	}
+
+	@Test
+	void contextLoads4() {
+		assertFalse(false);
+	}
+
+	@Test
+	void contextLoads5() {
+		assertTrue(true);
+	}
+
+
+	@Test
+	void contextLoads6() {
+		assertTrue(true);
+	}
+}
